812238afb0827a9f2f0450ef8b01d966b9b1c6f3
Tung Duong <laptung051@gmail.com>
2020-04-08 21:39:08 +0700

    Update Height and Weight Label when user changed value

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onDragSliderHeight(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f",sender.value) + "m";
+    }
+    
+    
+    @IBAction func onDragSliderWeight(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f",sender.value) + "kg";
+    }
+    
 }
 
